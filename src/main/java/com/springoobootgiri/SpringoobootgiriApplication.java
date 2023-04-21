84380a3e68606cb5ca708182e4eb6b191cbc93a0
Girish013 <girishhc199^@gmail.com>
2023-04-21 23:39:48 +0530

    First commit

---
+++ src/main/java/com/springoobootgiri/SpringoobootgiriApplication.java
@@ -0,0 +1,13 @@
+package com.springoobootgiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringoobootgiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringoobootgiriApplication.class, args);
+	}
+
+}
